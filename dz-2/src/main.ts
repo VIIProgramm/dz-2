dcc67da1c8bd84d3ff7fbd6963f58c2f7006606f
VIIProgramm <vlad1.1gordeev@gmail.com>
2023-08-03 21:25:46 +0300

    dz-2 n

--- dz-2/src/main.ts
+++ dz-2/src/main.ts
@@ -1,21 +1,38 @@
 import './style.css'
 
 // 1 Задание
-function stepen (n :number, k:number):number{
-  let cN = n
-  if (k > 0){
-    for(let i = 0; i < (k - 1); i++){
-      cN = cN * n 
-    }
-    return cN
-  }
-  if (k < 0){
-    for(let i = 0; i > (k + 1); i--){
-      cN = cN * n
-    }
-    return 1/cN
-  }
-  return 1
+function stepen (x :number, p:number) :any{
+  if (x == 0 && p <= 0) {return ("Невозможно возвести 0 в степень меньше 1")}
+  if (x == 1 || x == 0) {return x}
+  if (p > 1) {return x * stepen(x, --p)}
+  if (p < 1) {return 1 / x * stepen(x, ++p)}
+  return x
+} 
+
+
+console.log(stepen(2, 2))
+
+let date = new Date()
+
+function getLocalDay(day:Date){
+  let days = ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"]
+  return days[day.getDay()]
 }
 
-alert(stepen(2, 2))
+console.log(getLocalDay(date))
+
+
+//?------------------
+
+function getDateAgo( d:Date, n: number){
+  d.setDate(d.getDate() - n)
+  return d.getDate()
+} 
+
+console.log( getDateAgo(new Date(), 1) )
+console.log( getDateAgo(new Date(), 2) )
+console.log( getDateAgo(new Date(), 365))
+
+//?-----------------
+
+console.log(date.get)
